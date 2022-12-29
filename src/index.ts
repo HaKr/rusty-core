2e2e2c76c75656b99646bbb68fa1911d540d8778
Harry de Kroon <harry.de.kroon@gmail.com>
2022-12-29 13:33:27 +0100

    export of ResultPromise and resultFrom function

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 export { None, type Option, Some } from "./option/mod.ts";
-export { Err, Ok, type Result } from "./result/mod.ts";
+export {
+  Err,
+  Ok,
+  type Result,
+  resultFrom,
+  type ResultPromise,
+} from "./result/mod.ts";
