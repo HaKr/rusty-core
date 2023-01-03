1f3ecc4bbebd89ce7342387c59b63d31e543174c
Harry de Kroon <harry.de.kroon@gmail.com>
2023-01-03 13:38:08 +0100

    externalised optionFrom and OptionPromise

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-export { None, type Option, Some } from "./option/mod.ts";
+export {
+  None,
+  type Option,
+  optionFrom,
+  type OptionPromise,
+  Some,
+} from "./option/api.ts";
 export {
   Err,
   Ok,
